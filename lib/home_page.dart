6cfcff7e3e8c9308884b3cb3b6b71ae8d56be607
Alex UD <alex.ud@webhive.com.ua>
2025-04-09 22:14:04 +0300

    some fixes

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:youtube_caption_scraper/youtube_caption_scraper.dart';
 
@@ -27,12 +29,18 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> getSubtitles(String videoUrl) async {
-    final captionScraper = YouTubeCaptionScraper();
-    final captionTracks = await captionScraper.getCaptionTracks(videoUrl);
+    try {
+      final captionScraper = YouTubeCaptionScraper();
+      final captionTracks = await captionScraper.getCaptionTracks(videoUrl);
 
-    subtitles = await captionScraper.getSubtitles(captionTracks[0]);
+      subtitles = await captionScraper.getSubtitles(captionTracks[0]);
 
-    findByTheWord(wordEditingController.text);
+      findByTheWord(wordEditingController.text);
+    } catch (_) {
+      const snackBar = SnackBar(content: Text('Something went wrong'));
+
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
   }
 
   void findByTheWord(String word) {
@@ -60,69 +68,71 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: isLoading
-            ? Center(
-                child: CircularProgressIndicator(),
-              )
-            : SingleChildScrollView(
-                child: Column(
-                  children: [
-                    TextField(
-                      controller: searchEditingController,
-                      decoration: InputDecoration(
-                        hintText: 'url',
-                      ),
-                    ),
-                    TextField(
-                      controller: wordEditingController,
-                      decoration: InputDecoration(
-                        hintText: 'word',
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ElevatedButton(
-                        onPressed: () async {
-                          setState(() {
-                            isLoading = true;
-                          });
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextField(
+                controller: searchEditingController,
+                decoration: InputDecoration(
+                  hintText: 'url',
+                ),
+              ),
+              TextField(
+                controller: wordEditingController,
+                decoration: InputDecoration(
+                  hintText: 'word',
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    setState(() {
+                      isLoading = true;
+                    });
 
-                          if (searchEditingController.text.isEmpty) {
-                            setState(() {
-                              isLoading = false;
-                            });
-                            return;
-                          }
+                    if (searchEditingController.text.isEmpty) {
+                      setState(() {
+                        isLoading = false;
+                      });
+                      return;
+                    }
 
-                          String url = searchEditingController.text;
+                    String url = searchEditingController.text;
 
-                          await getSubtitles(url).then(
-                            (value) {
-                              setState(() {
-                                isLoading = false;
-                              });
-                            },
-                          );
-                        },
-                        child: Icon(Icons.search),
-                      ),
-                    ),
-                    if (finds.isEmpty) Text('nothing found'),
-                    Row(
+                    await getSubtitles(url).then(
+                      (value) {
+                        setState(() {
+                          isLoading = false;
+                        });
+                      },
+                    );
+                  },
+                  child: Icon(Icons.search),
+                ),
+              ),
+              isLoading
+                  ? CircularProgressIndicator()
+                  : Column(
                       children: [
+                        if (finds.isEmpty) Text('nothing found'),
                         if (finds.isNotEmpty)
-                          for (var item in time) Text(item),
-                        if (finds.isNotEmpty)
-                          for (var item in finds)
+                          for (int index = 0; index < finds.length; index++)
                             Padding(
                               padding: const EdgeInsets.only(left: 20),
-                              child: Text(item),
+                              child: Row(
+                                children: [
+                                  Text(time[index]),
+                                  SizedBox(width: 10),
+                                  Text(finds[index]),
+                                ],
+                              ),
                             ),
                       ],
                     ),
-                  ],
-                ),
-              ),
+            ],
+          ),
+        ),
       ),
     );
   }
